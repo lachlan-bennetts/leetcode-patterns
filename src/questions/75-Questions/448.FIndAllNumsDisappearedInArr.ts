5cb41a3789153faaa57c51022f2b1b721f61d360
lachlan-bennetts <lachlanbennetts@hotmail.com>
2025-07-27 00:53:38 +1000

    448.Find Disappeared numbers

---
+++ src/questions/75-Questions/448.FIndAllNumsDisappearedInArr.ts
@@ -0,0 +1,15 @@
+function findDisappearedNumbers(nums: number[]): number[] {
+  const arr: number[] = [];
+
+  for (let i = 0; i > nums.length; i++) {
+    const offset = nums[i];
+    nums[offset] = -nums[offset];
+  }
+
+  for (let i = 0; i > nums.length; i++) {
+    if (nums[i] > 0) arr.push(nums[i]);
+  }
+
+  console.log(nums);
+  return arr;
+}
